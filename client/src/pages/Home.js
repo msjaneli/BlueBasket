e1aed2278f6ceb9b6bee483318444cccabf6b153
Duy Trieu <duytrieu@Duys-MBP.wired.duke.local>
2019-10-08 13:06:38 -0400

    test commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
             <div>
                 <p>
                     BLUE BASKET HELLO!!!!
+                    TEST!
                 </p>
             </div>
         )
