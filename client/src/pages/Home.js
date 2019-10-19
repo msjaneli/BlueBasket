b0ecc51383af618afed2bc89798c3c5f98fc7754
msjaneli <jane.li1@duke.edu>
2019-10-19 17:14:07 -0400

    start homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import chooseNavbar from '../components/NavBar/chooseNavBar'
+import chooseNavbar from '../components/NavBar/chooseNavBar';
+import {ReactComponent as Splash} from '../resources/splash.svg';
+import '../styles/home.css';
 
 const mapStateToProps = (state) => ({
     user: state.session.user,
@@ -11,8 +13,17 @@ const mapStateToProps = (state) => ({
 class Home extends Component {
     render () {
         return (
+          <div>
             <div>
                 {chooseNavbar(this.props.user, this.props.authenticated, this.props.type)}
+            </div>
+            <div id="title">
+              It's time to <div style={{color:'#85B6FF', display:'inline'}}>reimagine food.</div>
+            </div>
+            <div id="splash" style={{height:'200px',
+      width: '500px'}} className="size">
+            <Splash/>
+            </div>
             </div>
         )
     }
