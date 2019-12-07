78decb5c8a130971096e2022de7bd9f0ace5eb6e
Eric <el172@duke.edu>
2019-12-07 15:48:40 -0500

    fixed the user order page

--- client/src/components/Profile/UserOrderCard.js
+++ client/src/components/Profile/UserOrderCard.js
@@ -17,16 +17,16 @@ class ListingCard extends Component {
     }
 
     componentDidMount = async () => {
-     setTimeout(async () => {
+     
        var { data } = await axios.get('/restaurant/'+this.props.order.rid);
        this.setState({
          restaurant: data
        })
-       var { data } = await axios.get('/listing/'+this.props.order.lids[0]);
+       var { data } = await axios.get('/listing/'+this.props.order.lids[this.props.orderIndex]);
        this.setState({
          listing: data
        })
-     }, 750);
+     
    }
 
     render() {
@@ -37,16 +37,16 @@ class ListingCard extends Component {
             </Row>
             <Row>
             <Col className="text-left"> <p className="user-order-card-name">{this.state.listing.name}</p></Col>
-            <Col><p className="user-order-card-quantity">Quantity: {this.props.order.quantities[0]}</p></Col>
+            <Col><p className="user-order-card-quantity">Quantity: {this.props.order.quantities[this.props.orderIdex]}</p></Col>
             <Col className="text-right"><p className="user-order-card-total">Total: ${this.props.order.total}</p></Col>
             </Row>
             <Row>
             <Col className="text-left"><p className="user-order-card-details">Type: {this.state.listing.type} | Allergens: {this.state.listing.allergens}</p></Col>
             </Row>
-            <Row>
-            <Col className="text-left"><p className="user-order-card-note">Note: {this.props.order.note}</p></Col>
+            <Row><Col className="text-left"><p className="user-order-card-details">Description: {this.state.listing.description}</p></Col>
             </Row>
-            <Row><Col className="text-left"><p className="user-order-card-description">{this.state.listing.description}</p></Col>
+            <Row>
+            <Col className="text-left"><p className="user-order-card-note">Note: {this.props.order.notes[this.props.orderIndex]}</p></Col>
             </Row>
           </Card>
         )
