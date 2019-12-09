0afbe9ae1060936c749249bb2f9bc6baa81a6be8
Neha Kota <neha.kota@duke.edu>
2019-12-09 12:40:18 -0500

    ShelterCheckout page created

---
+++ client/src/pages/Checkout/ShelterCheckout.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import '../../styles/checkout.css'
+
+// Component
+import { Container, Row, Col, Card, Button, Alert } from 'react-bootstrap';
+import CardCheckout from '../../components/Checkout/CardCheckout'
+
+// Selectors
+import * as checkoutSelectors from '../../selectors/checkoutSelectors'
+
+// Tools
+import isEmpty from '../../validation/isEmpty'
+import { connect } from 'react-redux'
+import { Elements } from 'react-stripe-elements'
+
+const mapStateToProps = (state) => ({
+    orders: checkoutSelectors.getCartByRestaurant(state),
+    subtotal: checkoutSelectors.getSubTotal(state),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+class ShelterCheckout extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            paymentType: null,
+        }
+    }
+
+
+    render (){
+
+        let selectPayment = (
+            <Card body>
+                <Row>
+                    <Col>
+                        <Button className="option-button" variant="donationsbalance">Donations Balance</Button>
+                    </Col>
+                </Row>
+
+                <style type="text/css">
+                    {`
+                        .btn-donationsbalance {
+                            background-color: #5282FF;
+                            color: white;
+                            font-weight: 400;
+                            margin-top: 2vh;
+                        }
+
+                        .btn-donationsbalance:hover {
+                            background-color: #a3bdff;
+                            color: white;
+                        }
+
+                    `}
+                    </style>
+            </Card>
+        )
+
+        console.log(this.props.orders);
+
+        let cartDetails = (
+            <Container>
+                {Object.keys(this.props.orders).map((rid, restaurantIndex) => {
+                            return(
+                                <Row key={restaurantIndex}>
+                                    <Col className = "text-left">
+                                        <Row>
+                                            <Col>
+                                                <h5 className="checkout-restauraunt-title">{this.props.orders[rid].restaurant}</h5>
+                                            </Col>
+                                        </Row>
+                                        {
+                                            this.props.orders[rid].lids.map((lid, index) => {
+                                                return (
+                                                  <Card className="checkout-item-card">
+                                                    <Row key = {index}>
+                                                        <Col>
+                                                        <h3 className="checkout-item-title">{this.props.orders[rid].names[index]}</h3>
+                                                        <h4 className="checkout-item-details">Quantity: {this.props.orders[rid].quantities[index]} |
+                                                            Price: ${this.props.orders[rid].prices[index]}</h4>
+                                                        <h4 className="checkout-item-note">Note: {this.props.orders[rid].notes[index]}</h4>
+
+                                                        </Col>
+                                                    </Row>
+                                                    </Card>
+                                                )
+                                            })
+                                        }
+                                    </Col>
+                                </Row>
+                            )
+                        })}
+                <Row>
+                    <Col className="text-left">
+                            <Row>
+                                <Col className ="text-left checkout-total">
+                                <p>Subtotal: ${this.props.subtotal}</p>
+                                <p>Tax: ${(this.props.subtotal * 0.0675).toFixed(2)}</p>
+                                <p>Total: ${(this.props.subtotal * 1.0675).toFixed(2)}</p>
+                                </Col>
+                            </Row>
+                    </Col>
+
+                </Row>
+
+            </Container>
+        )
+
+        if (isEmpty(this.props.orders)) {
+            return (
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col md={6}>
+                            <h2>Looks like your cart is empty!</h2>
+                        </Col>
+                    </Row>
+                </Container>
+            )
+
+        } else if (isEmpty(this.state.paymentType)) {
+            return (
+                <Container>
+                    <Row className="justify-content-md-center" style={{marginTop: '20vh'}}>
+                        <Col md={6}>
+                        {selectPayment}
+                        </Col>
+                    </Row>
+                </Container>
+            );
+        } else {
+            return (
+                <Container>
+                    <Row style={{marginTop: '5vh'}}>
+                        <Col md={5}>
+                            {cartDetails}
+                        </Col>
+                        <Col md={7}>
+                            <Elements>
+                                <CardCheckout/>
+                            </Elements>
+                        </Col>
+                    </Row>
+                </Container>
+            )
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShelterCheckout);
